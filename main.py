ff8a579db30d1f54c0ef0af732100dbea880b5da
Luiz Bicalho <felipe123pvh@gmail.com>
2024-11-25 21:06:14 -0400

    005

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask import request
+from flask import flash
+from flask import redirect
 
-app_Luiz = Flask(__name__)                   
+app_Luiz = Flask(__name__)      
+
+app_Luiz.config['SECRET_KEY'] = "ifro"
 
 @app_Luiz.route('/usuario')  
 
@@ -33,6 +38,26 @@ def base():
 @app_Luiz.route('/visualizar')
 def visualizar():
     return render_template('visualizar.html')
+#----------------------------------------------------------------
+@app_Luiz.route('/autenticar', methods=['GET', 'POST'])
+def autenticar():
+    usuario = request.form.get('nome_usuario')
+    senha = request.form.get('nome_senha')
+    
+    if usuario == 'admin' and senha == '123':
+        flash('Autenticado com sucesso!')
+        return redirect('/usuario')
+    else:
+        flash('Usuário ou senha inválidos!')
+        return redirect('/login')
+    # Aqui você pode validar o nome de usuário e senha
+    # e redirecionar para a página de home, caso seja válida
+    #return render_template('teste.html', usuario=usuario, senha=senha)
+    #return f"usuario: {usuario} e senha: {senha}"
+#----------------------------------------------------------------
+@app_Luiz.route('/login')
+def login():
+    return render_template('login.html')
 #----------------------------------------------------------------
 if __name__ == "__main__":  
     
